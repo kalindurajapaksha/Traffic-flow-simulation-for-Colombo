79a565e000df56af401c203a477ada72a257bd19
kalindurajapaksha <36252663+kalindurajapaksha@users.noreply.github.com>
2019-03-08 21:22:30 +0530

    Initial commit

---
+++ js/redirect.js
@@ -0,0 +1,42 @@
+
+//#################################################################
+// implementation of redirect buttons on the simulation html pages
+//#################################################################
+
+function myRedirectRing(){
+    window.location.href = "./ring.html";
+}
+
+function myRedirectOnramp(){
+    window.location.href = "./onramp.html";
+}
+
+function myRedirectOfframp(){
+    window.location.href = "./offramp.html";
+}
+
+function myRedirectRoadworks(){
+    window.location.href = "./roadworks.html";
+}
+
+function myRedirectUphill(){
+    window.location.href = "./uphill.html";
+}
+
+function myRedirectRouting(){
+    window.location.href = "./routing.html";
+}
+
+function myRedirectRoundabout(){
+    window.location.href = "./roundabout.html";
+}
+
+function myRedirectGame(){
+    window.location.href = "./routingGame.html";
+}
+
+function myRedirectTrafficlight(){
+    window.location.href = "./trafficlight.html";
+}
+
+
